aaf71b5308cc4209a6e200e212a0563c0351a4fb
Omanjusan <Omanjusan@proton.me>
2026-07-21 03:29:34 +0900

    js→tsが正になったのでjsを追跡対象外に入れた

--- background.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/* 開発用: インストール(web-ext run の一時読み込み含む)時にパネルをタブで開く */
-browser.runtime.onInstalled.addListener(() => {
-    browser.tabs.create({ url: browser.runtime.getURL("panel/panel.html") });
-});
-//# sourceMappingURL=background.js.map
